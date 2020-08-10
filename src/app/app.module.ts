d3c3fdb12052480bd29b37cf2b1db668e741dca7
jorgesoto98 <jorgesoto98@users.noreply.github.com>
2020-08-10 10:41:49 -0500

    Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
